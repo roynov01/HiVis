b913aee4dac7d407f5aa2d530f2f7c7461a273b3
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2025-03-18 13:55:37 +0200

    .

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,10 +18,10 @@ extensions = [
 
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
 
-#autodoc_typehints = "none"
-#autodoc_docstring_signature = False
 
 myst_enable_extensions  = [
     "dollarmath",
