18dcce69d8b8bc61df41fadf72f312bca48ac05d
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2025-08-03 16:48:24 +0300

    updated method order Aggregation

--- HiVis/Aggregation.py
+++ HiVis/Aggregation.py
@@ -67,7 +67,168 @@ class Aggregation:
         if geojson_agg_path:
             self.import_geometry(geojson_agg_path,object_type="cell")
 
+    
+    @property
+    def columns(self):
+        return self.adata.obs.columns.copy()
+    
+    
+    def combine(self, other):
+        '''
+        Combines two Aggregation objects into a single adata. Spatial plots and analysis will be disabled.
+        '''
+        return self + other
+    
+    
+    def copy(self, new_name=None, new_out_path=False, full=False):
+        '''
+        Creates a deep copy of the instance
+        if new_name is specified, renames the object and changes the path_output.
+        If full is False, the name will be added to the current (previous) name.
+        
+        **Returns** new Aggregation instance
+        '''
+        new = deepcopy(self)
+        new.viz = self.viz
+        gc.collect()
+        new = deepcopy(self)
+        if new_name:
+            new.rename(new_name, new_out_path=new_out_path, full=full)
+        return new
+    
+    
+    def export_h5(self, path=None):
+        '''
+        Exports the adata. 
+        
+        Parameters:
+            * path (str) - path to save the h5 file. If None, will save to path_output
+            
+        **Returns** path where the file was saved (str)
+        '''
+        print(f"SAVING [{self.name}]")
+        if not path:
+            path = f"{self.path_output}/{self.name}.h5ad"
+        self.adata.write(path)
+        return path
+    
+    
+    def export_to_matlab(self, path=None, layer=None):
+        '''
+        Exports gene names, data (sparse matrix) and metadata to a .mat file.
+        
+        Parameters:
+            * path (str) - path of mat file to save to
+            * layer - which layer from the adata to use
+            
+        **Returns** path of mat file
+        '''
+        var_names = self.adata.var_names.to_numpy()  
+        if 'X_umap' in self.adata.obsm:
+            self.adata.obs['UMAP_1'] = self.adata.obsm['X_umap'][:, 0]  
+            self.adata.obs['UMAP_2'] = self.adata.obsm['X_umap'][:, 1]  
+            
+        obs = self.adata.obs.copy()
+        obs["Cell_ID"] = obs.index.tolist()
+        
+        # Shorten long column names in obs
+        def shorten_col_names(columns, max_len=28):
+            seen_names = {}
+            rename_dict = {}
+            for col in columns:
+                if len(col) > max_len:
+                    base_name = col[:max_len]  
+                    count = seen_names.get(base_name, 0)
+                    new_name = f"{base_name}_{count}"
+                    seen_names[base_name] = count + 1
+                    rename_dict[col] = new_name
+            return rename_dict
+        
+        rename_dict = shorten_col_names(obs.columns)
+        obs = obs.rename(columns=rename_dict)
+        
+        def remove_non_ascii(d):
+            return {re.sub(r'[^\x00-\x7F]+', '_', k): v for k, v in d.items()}
+        
+        obs = obs.to_dict(orient='list')  
+        obs = remove_non_ascii(obs)
 
+        if not path:
+            path = f"{self.path_output}/matlab"
+            if not os.path.exists(path):
+                os.makedirs(path)
+            path = f"{path}/{self.name}.mat"
+        print("[Saving mat file]")
+        mat = self.adata.X if layer is None else self.adata.layers[layer]
+        scipy.io.savemat(path, {"genes": var_names, "mat":mat ,"metadata":obs})
+        self.adata.obs.to_csv(path.replace(".mat","metadata.csv"))
+        return path
+    
+    
+    def get(self, what, cropped=False, geometry=False, layer=None):
+        '''
+        get a vector from data (a gene) or metadata (from obs or var). or subset the object.
+        
+        Parameters:
+            * what - if string, will get data or metadata. \
+                     else, will return a new Aggregation object that is spliced. \
+                     the splicing is passed to the self.adata.
+            * cropped (bool) - get the data from the adata_cropped after plotting spatial
+            * geometry (bool) - include only objects which have geometry
+            
+        **Returns**: either np.array of data or, if subsetting, a new Aggregation instance
+        '''
+        
+        adata = self.adata_cropped if cropped else self.adata
+        if geometry and self.plot.geometry is not None:
+            adata = adata[adata.obs.index.isin(self.plot.geometry.index)]
+        if isinstance(what, str):  # Easy access to data or metadata arrays
+            if what in adata.obs.columns:  # Metadata from OBS
+                column_data = adata.obs[what]
+                if column_data.dtype.name == 'category':
+                    return column_data.astype(str).values
+                return column_data.values
+            elif what in adata.var.index:  # A gene
+                gene_data = adata[:, what].X if layer is None else adata[:, what].layers[layer]
+                return np.array(gene_data.todense().ravel()).flatten()
+            elif what in adata.var.columns:  # Gene metadata from VAR
+                column_data = adata.var[what]
+                if column_data.dtype.name == 'category':
+                    return column_data.astype(str).values
+                return column_data.values
+            
+            obs_cols_lower = adata.obs.columns.str.lower()
+            if what.lower() in obs_cols_lower:
+                col_name = adata.obs.columns[obs_cols_lower.get_loc(what.lower())]
+                column_data = adata.obs[col_name]
+                if column_data.dtype.name == 'category':
+                    return column_data.astype(str).values
+                return column_data.values
+            elif self.viz.organism == "mouse" and (what.lower().capitalize() in adata.var.index):
+                gene_name = what.lower().capitalize()
+                gene_data = adata[:, gene_name].X if layer is None else adata[:, gene_name].layers[layer]
+                return  np.array(gene_data.todense().ravel()).flatten()
+            elif self.viz.organism == "human" and (what.upper() in adata.var.index):
+                gene_name = what.lower().upper()
+                gene_data = adata[:, gene_name].X if layer is None else adata[:, gene_name].layers[layer]
+                return  np.array(gene_data.todense().ravel()).flatten()
+            var_cols_lower = adata.var.columns.str.lower()
+            if what.lower() in var_cols_lower:
+                col_name = adata.var.columns[var_cols_lower.get_loc(what.lower())]
+                column_data = adata.var[col_name]
+                if column_data.dtype.name == 'category':
+                    return column_data.astype(str).values
+                return column_data.values
+        else:
+            # Create a new Aggregation object based on adata subsetting
+            return self.subset(what)
+        
+        
+    def head(self, n=5):
+        '''**Returns** Aggregation.adata.obs.head(n), where n is number of rows'''
+        return self.adata.obs.head(n) 
+    
+    
     def import_geometry(self, geojson_path, object_type="cell"):
         '''
         Adds "geometry" column to self.adata.obs, based on Geojson exported from Qupath.
@@ -182,64 +343,18 @@ class Aggregation:
             for o in obsm:
                 self.adata.obsm[o] = adata.obsm[o].copy()
         
-            
-    def get(self, what, cropped=False, geometry=False, layer=None):
+    def rename(self, new_name: str, new_out_path=True, full=False):
         '''
-        get a vector from data (a gene) or metadata (from obs or var). or subset the object.
-        
-        Parameters:
-            * what - if string, will get data or metadata. \
-                     else, will return a new Aggregation object that is spliced. \
-                     the splicing is passed to the self.adata.
-            * cropped (bool) - get the data from the adata_cropped after plotting spatial
-            * geometry (bool) - include only objects which have geometry
-            
-        **Returns**: either np.array of data or, if subsetting, a new Aggregation instance
+        Renames the object and changes the path_output.
+        If full is False, the name will be added to the current (previous) name
         '''
-        
-        adata = self.adata_cropped if cropped else self.adata
-        if geometry and self.plot.geometry is not None:
-            adata = adata[adata.obs.index.isin(self.plot.geometry.index)]
-        if isinstance(what, str):  # Easy access to data or metadata arrays
-            if what in adata.obs.columns:  # Metadata from OBS
-                column_data = adata.obs[what]
-                if column_data.dtype.name == 'category':
-                    return column_data.astype(str).values
-                return column_data.values
-            elif what in adata.var.index:  # A gene
-                gene_data = adata[:, what].X if layer is None else adata[:, what].layers[layer]
-                return np.array(gene_data.todense().ravel()).flatten()
-            elif what in adata.var.columns:  # Gene metadata from VAR
-                column_data = adata.var[what]
-                if column_data.dtype.name == 'category':
-                    return column_data.astype(str).values
-                return column_data.values
-            
-            obs_cols_lower = adata.obs.columns.str.lower()
-            if what.lower() in obs_cols_lower:
-                col_name = adata.obs.columns[obs_cols_lower.get_loc(what.lower())]
-                column_data = adata.obs[col_name]
-                if column_data.dtype.name == 'category':
-                    return column_data.astype(str).values
-                return column_data.values
-            elif self.viz.organism == "mouse" and (what.lower().capitalize() in adata.var.index):
-                gene_name = what.lower().capitalize()
-                gene_data = adata[:, gene_name].X if layer is None else adata[:, gene_name].layers[layer]
-                return  np.array(gene_data.todense().ravel()).flatten()
-            elif self.viz.organism == "human" and (what.upper() in adata.var.index):
-                gene_name = what.lower().upper()
-                gene_data = adata[:, gene_name].X if layer is None else adata[:, gene_name].layers[layer]
-                return  np.array(gene_data.todense().ravel()).flatten()
-            var_cols_lower = adata.var.columns.str.lower()
-            if what.lower() in var_cols_lower:
-                col_name = adata.var.columns[var_cols_lower.get_loc(what.lower())]
-                column_data = adata.var[col_name]
-                if column_data.dtype.name == 'category':
-                    return column_data.astype(str).values
-                return column_data.values
+        if full:
+            self.name = new_name
         else:
-            # Create a new Aggregation object based on adata subsetting
-            return self.subset(what)
+            self.name = f"{self.viz.name}_{new_name}"
+        if new_out_path:
+            self.path_output = self.viz.path_output + f"/{new_name}"        
+    
         
     def subset(self, what=(slice(None), slice(None))):
         '''
@@ -255,18 +370,12 @@ class Aggregation:
             adata.layers[layer] = self.adata.layers[layer][what].copy()
         return Aggregation(self.viz, adata, name=self.name)
     
-    def __getitem__(self, what):
-        '''get a vector from data (a gene) or metadata (from obs or var). or subset the object.'''
-        item = self.get(what, cropped=False)
-        if item is None:
-            raise KeyError(f"[{what}] isn't in data or metadatas")
-        return item
     
-    def __contains__(self, what):
-        if (what in self.adata.obs) or (what in self.adata.var):
-            return True
-        return False
-     
+    @property
+    def shape(self):
+        return self.adata.shape
+    
+    
     def sync(self, what: str):
         '''
         Transfers metadata assignment from the Aggregation to the spots.
@@ -280,54 +389,19 @@ class Aggregation:
         if agg_id_col not in self.viz.adata.obs.columns:
             raise KeyError(f"'{agg_id_col}' does not exist in HiViz.adata.obs.")
         mapping = self.adata.obs[what]
-        self.viz.adata.obs[what] = self.viz.adata.obs[agg_id_col].map(mapping)
+        self.viz.adata.obs[what] = self.viz.adata.obs[agg_id_col].map(mapping)   
         
-    def export_h5(self, path=None):
+    
+    def update(self):
         '''
-        Exports the adata. 
-        
-        Parameters:
-            * path (str) - path to save the h5 file. If None, will save to path_output
-            
-        **Returns** path where the file was saved (str)
+        Updates the methods in the instance. 
+        Should be used after modifying the source code in the class
         '''
-        print(f"SAVING [{self.name}]")
-        if not path:
-            path = f"{self.path_output}/{self.name}.h5ad"
-        self.adata.write(path)
-        return path
+        HiVis_utils.update_instance_methods(self)
+        HiVis_utils.update_instance_methods(self.plot)
+        HiVis_utils.update_instance_methods(self.analysis)
+        _ = gc.collect()
     
-   
-    @property
-    def shape(self):
-        return self.adata.shape
-    
-    def __str__(self):
-        s = f"# Aggregation # {self.name} #\n\n"
-        s += f"# Parent: {self.viz.name} #\n"
-        s += f"\tSize: {self.adata.shape[0]} x {self.adata.shape[1]}\n"
-        s += '\nobs: '
-        s += ', '.join(list(self.adata.obs.columns))
-        if not self.adata.var.columns.empty:
-            s += '\n\nvar: '
-            s += ', '.join(list(self.adata.var.columns))
-        layers = list(self.adata.layers.keys())
-        if layers:
-            s += '\n\nlayers: '
-            s += ', '.join(layers)
-        return s
-    
-    def __repr__(self):
-        # s = f"Aggregation[{self.name}]"
-        s = self.__str__()
-        return s
-    
-    
-    def combine(self, other):
-        '''
-        Combines two Aggregation objects into a single adata. Spatial plots and analysis will be disabled.
-        '''
-        return self + other
     
     def __add__(self, other):
         '''Combines two Aggregation objects into a single Aggregation. Some methods will be disabled'''
@@ -349,6 +423,13 @@ class Aggregation:
             new_obj.sync = _disabled_method
         return new_obj
     
+    
+    def __contains__(self, what):
+        if (what in self.adata.obs) or (what in self.adata.var):
+            return True
+        return False
+    
+    
     def __delitem__(self, key):
         '''Deletes metadata'''
         if isinstance(key, str):
@@ -360,7 +441,22 @@ class Aggregation:
                 raise KeyError(f"'{key}' not found in adata.obs")
         else:
             raise TypeError(f"Key must be a string, not {type(key).__name__}")
+            
+            
+    def __getitem__(self, what):
+        '''get a vector from data (a gene) or metadata (from obs or var). or subset the object.'''
+        item = self.get(what, cropped=False)
+        if item is None:
+            raise KeyError(f"[{what}] isn't in data or metadatas")
+        return item
     
+    
+    def __repr__(self):
+        # s = f"Aggregation[{self.name}]"
+        s = self.__str__()
+        return s
+    
+
     def __setitem__(self, key, value):
         if len(value) == self.adata.shape[0]:
             self.adata.obs[key] = value
@@ -369,100 +465,19 @@ class Aggregation:
         else:
             raise ValueError("Values must be in the length of OBS or VAR")
     
-    def update(self):
-        '''
-        Updates the methods in the instance. 
-        Should be used after modifying the source code in the class
-        '''
-        HiVis_utils.update_instance_methods(self)
-        HiVis_utils.update_instance_methods(self.plot)
-        HiVis_utils.update_instance_methods(self.analysis)
-        _ = gc.collect()
     
-    def head(self, n=5):
-        '''**Returns** Aggregation.adata.obs.head(n), where n is number of rows'''
-        return self.adata.obs.head(n) 
+    def __str__(self):
+        s = f"# Aggregation # {self.name} #\n\n"
+        s += f"# Parent: {self.viz.name} #\n"
+        s += f"\tSize: {self.adata.shape[0]} x {self.adata.shape[1]}\n"
+        s += '\nobs: '
+        s += ', '.join(list(self.adata.obs.columns))
+        if not self.adata.var.columns.empty:
+            s += '\n\nvar: '
+            s += ', '.join(list(self.adata.var.columns))
+        layers = list(self.adata.layers.keys())
+        if layers:
+            s += '\n\nlayers: '
+            s += ', '.join(layers)
+        return s
     
-    @property
-    def columns(self):
-        return self.adata.obs.columns.copy()
-    
-    def copy(self, new_name=None, new_out_path=False, full=False):
-        '''
-        Creates a deep copy of the instance
-        if new_name is specified, renames the object and changes the path_output.
-        If full is False, the name will be added to the current (previous) name.
-        
-        **Returns** new Aggregation instance
-        '''
-        new = deepcopy(self)
-        new.viz = self.viz
-        gc.collect()
-        new = deepcopy(self)
-        if new_name:
-            new.rename(new_name, new_out_path=new_out_path, full=full)
-        return new
-    
-    def rename(self, new_name: str, new_out_path=True, full=False):
-        '''
-        Renames the object and changes the path_output.
-        If full is False, the name will be added to the current (previous) name
-        '''
-        if full:
-            self.name = new_name
-        else:
-            self.name = f"{self.viz.name}_{new_name}"
-        if new_out_path:
-            self.path_output = self.viz.path_output + f"/{new_name}"
-    
-    def export_to_matlab(self, path=None, layer=None):
-        '''
-        Exports gene names, data (sparse matrix) and metadata to a .mat file.
-        
-        Parameters:
-            * path (str) - path of mat file to save to
-            * layer - which layer from the adata to use
-            
-        **Returns** path of mat file
-        '''
-        var_names = self.adata.var_names.to_numpy()  
-        if 'X_umap' in self.adata.obsm:
-            self.adata.obs['UMAP_1'] = self.adata.obsm['X_umap'][:, 0]  
-            self.adata.obs['UMAP_2'] = self.adata.obsm['X_umap'][:, 1]  
-            
-        obs = self.adata.obs.copy()
-        obs["Cell_ID"] = obs.index.tolist()
-        
-        # Shorten long column names in obs
-        def shorten_col_names(columns, max_len=28):
-            seen_names = {}
-            rename_dict = {}
-            for col in columns:
-                if len(col) > max_len:
-                    base_name = col[:max_len]  
-                    count = seen_names.get(base_name, 0)
-                    new_name = f"{base_name}_{count}"
-                    seen_names[base_name] = count + 1
-                    rename_dict[col] = new_name
-            return rename_dict
-        
-        rename_dict = shorten_col_names(obs.columns)
-        obs = obs.rename(columns=rename_dict)
-        
-        def remove_non_ascii(d):
-            return {re.sub(r'[^\x00-\x7F]+', '_', k): v for k, v in d.items()}
-        
-        obs = obs.to_dict(orient='list')  
-        obs = remove_non_ascii(obs)
-
-        if not path:
-            path = f"{self.path_output}/matlab"
-            if not os.path.exists(path):
-                os.makedirs(path)
-            path = f"{path}/{self.name}.mat"
-        print("[Saving mat file]")
-        mat = self.adata.X if layer is None else self.adata.layers[layer]
-        scipy.io.savemat(path, {"genes": var_names, "mat":mat ,"metadata":obs})
-        self.adata.obs.to_csv(path.replace(".mat","metadata.csv"))
-        return path
-        
