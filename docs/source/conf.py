434ea55aa69dc07d7fc94739c8f939677849fe8f
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2025-11-27 16:18:12 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',  
+#    'sphinx.ext.viewcode',  
     'myst_parser',
     'sphinx.ext.linkcode',  
 ]
