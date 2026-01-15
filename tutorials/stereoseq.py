cb78dff5fb3a0aa4e4c01659598b1636071dc319
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2026-01-15 13:28:50 +0200

    Refactor brain StereoSeq initialization

--- tutorials/stereoseq.py
+++ tutorials/stereoseq.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 14 14:29:56 2026
-
-@author: royno
-"""
-
-import matplotlib.pyplot as plt
 from HiVis import HiVis
-
-
 
 # Brain StereoSeq data was downloaded from: https://spateo-release.readthedocs.io/en/latest/tutorials/notebooks/1_cell_segmentation/stain_segmentation.html
 # The data is from Chen et al. 2022 (PMID: 35512705), processed by the STOmics SAW software, available from Qiu et al. 2024 (PMID: 39532097).
@@ -23,17 +13,5 @@ bin_size_um = 5
 microns_per_pixel = 0.5 
 
 brain = HiVis.new_stereoseq(
-    path_transcripts=transcripts_file,
-    path_image=image_file,
-    bin_size_um=bin_size_um,  
-    name=name,
-    path_output=path_output,
-    fluorescence=fluorescence,
-    microns_per_pixel=microns_per_pixel,
-    flip_img=True
-)
-
-# ax = brain.plot.spatial("nUMI",exact=True,img_resolution="full",save=True,title="",show_zeros=1,scalebar={"text":False})
-# brain.plot.spatial(exact=True,img_resolution="full",legend=False,save=True,scalebar={"text":False})
-
-
+    path_transcripts=transcripts_file, path_image=image_file, bin_size_um=bin_size_um, name=name,
+    path_output=path_output, fluorescence=fluorescence, microns_per_pixel=microns_per_pixel, flip_img=True)
