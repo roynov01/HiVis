054c4b47fca3213719bea25aa3a868026861b447
Roy Novoselsky <84639293+roynov01@users.noreply.github.com>
2026-03-26 18:52:13 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HiVis", 
-    version="0.9",
+    version="1.0.0",
     author="Roy Novoselsky",
     author_email="roy.novoselsky@weizmann.ac.il",
     description="A user friendly tool for working with VisiumHD data",
